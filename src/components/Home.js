ae0857281d3b7928916ac98d0775b6080e4a9c60
Niket <40358714+nikets40@users.noreply.github.com>
2021-10-16 03:33:54 +0530

    ADD: navbar

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+function Home() {
+  return (
+    <Container>
+      <Nav />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div``;
---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+function Nav() {
+  return (
+    <NavBar>
+      <Logo>
+        <img alt="log" src="/images/logo.svg" />
+      </Logo>
+
+      <NavMenu>
+        <a href="/">Home</a>
+        <a href="find-a-doctor">Find a doctor</a>
+        <a href="apps">Apps</a>
+        <a href="testimonials">Testimonials</a>
+        <a href="about-us">About us</a>
+      </NavMenu>
+    </NavBar>
+  );
+}
+
+export default Nav;
+
+const NavBar = styled.div`
+  display: flex;
+  height: 70px;
+  max-width: 1024px;
+  margin: auto;
+  padding: 0 20px;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.div`
+  cursor: pointer;
+`;
+
+const NavMenu = styled.div`
+  a:first-child {
+    font-weight: 900;
+    color: #1f1534;
+  }
+  a {
+    margin-left: 1.6rem;
+    font-size: 18px;
+    color: rgba(31, 21, 52, 0.5);
+    font-weight: 400;
+    transition: all 250ms;
+
+    &:hover {
+        color: rgba(31, 21, 52, 1);
+    font-weight: 700;
+    }
+  }
+`;
